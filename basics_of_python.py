68fc923d4a1f0e178fbac5b7c33d1a6f29ff2b5e
libindev <libin.developer@gmail.com>
2019-03-23 11:59:54 +0530

    Initial commit

---
+++ basics_of_python.py
@@ -0,0 +1,14 @@
+
+
+
+def hello(x,y):
+
+    if x > y:
+        print(x,"is greater than ",y)
+    elif y > x:
+        print(x,"is less than",y)
+    else:
+        print(x, "is equal to ", y)
+
+
+hello(100, 45)
